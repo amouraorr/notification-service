30c8cf990269d5ef5d275f49e55550b3e3593790
Alex Mourão <amouraorr@yahoo.com.br>
2025-10-17 13:42:24 -0300

    notification-service primeiro commit

---
+++ src/test/java/com/alex/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
